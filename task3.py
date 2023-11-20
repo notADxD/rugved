b72fbd5f9f16918e0df2bb1a0debf948358d4684
notADxD <149404340+notADxD@users.noreply.github.com>
2023-11-20 10:44:47 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+import numpy as np
+
+# Define the upper and lower bounds for green in HSV color space
+lower_green = np.array([40, 50, 50])
+upper_green = np.array([80, 255, 255])
+
+video = cv.VideoCapture('green.mp4')
+
+while True:
+    ret, frame = video.read()
+    if not ret:
+        break
+
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+    mask = cv.inRange(hsv, lower_green, upper_green)
+
+    mask = cv.erode(mask, None, iterations=2)
+    mask = cv.dilate(mask, None, iterations=2)
+
+    contours, _ = cv.findContours(mask.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        area = cv.contourArea(contour)
+        if area > 100:
+            cv.drawContours(frame, [contour], -1, (0, 255, 0), 2)
+
+    cv.imshow('Original', frame)
+
+    if cv.waitKey(20) & 0xFF == ord('q'):
+        break
+
+video.release()
+cv.destroyAllWindows()
